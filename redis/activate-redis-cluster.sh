583697d3f4f2a6ad917760b0c407059461bb1d6f
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2023-04-10 01:56:07 +0100

    Redis Cluster - k8s configuration

---
+++ redis/activate-redis-cluster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Find ClusterIPs of Redis nodes
+export REDIS_NODES=$(kubectl get pods  -l app=redis-cluster -n default -o json | jq -r '.items | map(.status.podIP) | join(":6379 ")'):6379
+# Activate the Redis cluster
+kubectl exec -it redis-cluster-0 -n default -- redis-cli --cluster create --cluster-replicas 1 ${REDIS_NODES}
+# Check if all went well
+for x in $(seq 0 5); do echo "redis-cluster-$x"; kubectl exec redis-cluster-$x -n default -- redis-cli role; echo; done
